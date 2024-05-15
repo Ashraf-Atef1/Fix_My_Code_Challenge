bd465f4161698e2ae28f3e19f9351e3b210d690a
Ashraf-Atef1 <144985320+Ashraf-Atef1@users.noreply.github.com>
2024-05-16 00:57:22 +0300

    Update AllPostActions.js

--- 0x01-challenge/react-blog/src/actions/AllPostActions.js
+++ 0x01-challenge/react-blog/src/actions/AllPostActions.js
@@ -53,12 +53,12 @@ class AllPostActions {
 
         var AllPostStore = require('../stores/AllPostStore');
         var state = AllPostStore.getState();
-        if(state.numberOfPosts == 0) {
+        if(state.getNumberOfPosts == 0) {
             request.get(config.baseUrl+'/ajax/getNumberOfPosts',function(err,response) {
-                self.actions.update_numberOfPosts(response.body.numberOfPosts);
+                self.actions.updateNumberOfPosts(response.body.numberOfPosts);
             });
         } else {
-            this.actions.update_numberOfPosts(state.numberOfPosts);
+            this.actions.updateNumberOfPosts(state.getNumberOfPosts);
         }
     }
 
